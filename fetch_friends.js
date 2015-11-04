c935e9fb5521583ef88614091fca2fcc18e25676
Dan Silver <dannysilver3@gmail.com>
2015-11-03 18:58:20 -0600

    screen name -> user id

--- fetch_friends.js
+++ fetch_friends.js
@@ -9,24 +9,34 @@ var T = new Twit({
   access_token_secret: keys.access_token_secret
 });
 
+
 var data_directory = 'data/'
+var queue = []
 
-function fetchFollowerIds(screen_name, cursor, limit) {
-	if (limit < 0)
-		return;
+function fetchFriendIds(user_id, cursor, limit, callback) {
+	if (limit < 0) {
+		console.log("limit", limit, "reached")
+		return callback();
+	}
 
-	T.get('followers/ids', { screen_name: screen_name, count: 5000, cursor: cursor, stringify_ids: true},  function(error, data, response) {
+	T.get('friends/ids', { user_id: user_id, count: 5000, cursor: cursor, stringify_ids: true},  function(error, data, response) {
 		if (error) {
 			console.error(error)
-			return
+			return callback()
 		}
 
 		console.log(data.ids)
-		appendLineSeparatedToFile(screen_name + '.txt', data.ids)
+		appendLineSeparatedToFile(user_id + '.txt', data.ids)
 
-		// get next batch of ids
-		if (data['next_cursor'] != 0)
-			fetchFollowerIds(screen_name, data['next_cursor'], limit - data.ids.length)
+		for (var i=0; i<data.ids.length; i++)
+			queue.push(data.ids[i])
+
+		// get next batch of ids if their is another page
+		if (data['next_cursor'] == 0) {
+			console.log('End of data reached for', user_id)
+			callback()
+		} else
+			fetchFriendIds(user_id, data['next_cursor'], limit - data.ids.length)
 	})
 }
 
@@ -41,4 +51,18 @@ function appendLineSeparatedToFile(filename, array) {
 }
 
 // screenname, twitter terminator, limit
-fetchFollowerIds('mizzou', -1, 20*1000)
+
+//seed queue with 'mizzou'
+
+function processQueue() {
+	if (queue.length == 0)
+		return
+
+	var item = queue.splice(0,1)
+	fetchFriendIds(item, -1, 20*1000, function() {
+		processQueue()
+	})
+}
+
+queue.push('23620660') //@mizzou
+processQueue()
